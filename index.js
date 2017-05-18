a0301c3e8f433a801365eca232d5a190272d0a92
sdkevorkian <sdkevorkian@gmail.com>
2017-05-18 13:51:13 -0700

    GET all stops route is working, users functional

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-var mongoose = require("mongoose");
-=======
 /* global process, module */
 
 var express = require('express');
@@ -9,7 +6,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect('mongodb://localhost/app');
+mongoose.connect('mongodb://localhost/stop_alarm');
 // may need to change this path
 
 app.use(bodyParser.json());
@@ -17,8 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('API NAME', require('./controllers/CONTROLLERS NAME'))
-    // need to change path above
+app.use('/api/users', require('./controllers/users.js'));
+app.use('/api/stops', require('./controllers/stops.js'));
+// need to change path above
+
+
+
 
 // * gets all routes
 app.get('/*', function(req, res) {
@@ -28,4 +29,3 @@ app.get('/*', function(req, res) {
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
->>>>>>> e78a38b8ea0bfe0865e53db0c51d4b35f32cfa50

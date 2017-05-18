410fc22358156e33c0607acc9e3751ece51ac5b0
alainabuzas <alaina.buzas@gmail.com>
2017-05-18 13:50:09 -0700

    views set up and public-appjs

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-var mongoose = require("mongoose");
-=======
 /* global process, module */
 
 var express = require('express');
@@ -9,7 +6,10 @@ var path = require('path');
 var mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect('mongodb://localhost/app');
+var User = require('./models/user');
+mongoose.connect('mongodb://localhost/stop_alarm');
+
+// mongoose.connect('mongodb://localhost/app');
 // may need to change this path
 
 app.use(bodyParser.json());
@@ -17,8 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('API NAME', require('./controllers/CONTROLLERS NAME'))
-    // need to change path above
+app.use('/api/users', require('./controllers/users.js'));
+
+// app.use('API NAME', require('./controllers/CONTROLLERS NAME'))
+// need to change path above
 
 // * gets all routes
 app.get('/*', function(req, res) {
@@ -28,4 +30,3 @@ app.get('/*', function(req, res) {
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
->>>>>>> e78a38b8ea0bfe0865e53db0c51d4b35f32cfa50

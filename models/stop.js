769988847fe2a900bdca2705c3621d122a175b96
sdkevorkian <sdkevorkian@gmail.com>
2017-05-18 12:08:03 -0700

    attempting to make route and stop models...

--- models/stop.js
+++ models/stop.js
@@ -1,1 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+var StopSchema = new Schema({
+    stop_id: { type: Number },
+    stop_name: { type: String },
+    stop_lat: { type: Number },
+    stop_lon: { type: Number },
+    routes: [{ type: Number, ref: "Route" }]
+});
+
+module.exports = mongoose.model('Stop', StopSchema);

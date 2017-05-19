a848edb3923813e2c0cfdda9a30e309d9445a891
alainabuzas <alaina.buzas@gmail.com>
2017-05-18 17:37:49 -0700

    hopefully on the same page

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,24 +3,21 @@ angular.module('BusCtrls', ['BusServices'])
 
     }])
     .controller('AllStopsCtrl', ['$scope', 'BusStop', function($scope, BusStop) {
-            $scope.searchStops = function() {
-                var searchTerm = $scope.stopSearch.toLowerCase();
-                BusStop.showAllStops().then(function(res) {
-                    var stopsFiltered = res.data.filter(function(stop) {
-                        var thisStop = stop.stop_name.toLowerCase();
-                        return thisStop.includes(searchTerm);
-                    });
-                    $scope.stops = stopsFiltered;
-                }).catch(function(res) {
-                    $scope.stops = res;
+        $scope.searchStops = function() {
+            // puts to lowercase so that user can type as they want
+            var searchTerm = $scope.stopSearch.toLowerCase();
+            BusStop.showAllStops().then(function(res) {
+                var stopsFiltered = res.data.filter(function(stop) {
+                    var thisStop = stop.stop_name.toLowerCase();
+                    return thisStop.includes(searchTerm);
                 });
-            };
-            $scope.stops = stopsFiltered;
-            console.log(stopsFiltered.length);
-        }).catch(function(res) {
-            $scope.stops = res;
-        });
+                $scope.stops = stopsFiltered;
+            }).catch(function(res) {
+                $scope.stops = res;
+            });
+        };
     }])
+
 .controller('ShowCtrl', ['$scope', function($scope) {
 
     }])
